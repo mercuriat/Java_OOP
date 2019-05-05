2b0ab777d111a2847c778cae121bf05c1e384176
赵天啸 <zhaotianxiao@zhaotianxiaodeMacBook-Pro.local>
2019-05-05 21:25:01 +0900

    Complete VigenereProgram

--- VigenereProgram/VigenereBreaker.java
+++ VigenereProgram/VigenereBreaker.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import edu.duke.*;
+import java.io.*;
 
 public class VigenereBreaker {
     public String sliceString(String message, int whichSlice, int totalSlices) {
@@ -25,23 +26,146 @@ public class VigenereBreaker {
         return key;
     }
 
-    public void breakVigenere () {
+    public String breakVigenere(int klength, String encrypted, char commonletter) {
         //WRITE YOUR CODE HERE
-        FileResource fr = new FileResource("secretmessage1.txt");
-        String encrypted = fr.asString();
-        int klength = 4;
-        int[] key = tryKeyLength(encrypted, klength, 'e');
+        int[] key = tryKeyLength(encrypted, klength, commonletter);
 
         VigenereCipher vc = new VigenereCipher(key);
         String decrypted = vc.decrypt(encrypted);
         
         //Mainly for the first line
-        System.out.println(decrypted.substring(0,100));
+        //System.out.println(decrypted.substring(0,100));
         
         //Print out the key
-        for(int i = 0; i < klength; i++){
-            System.out.println(key[i]);
-        }        
+        //for(int i = 0; i < klength; i++){
+        //    System.out.println(key[i]);
+        //}
+        return decrypted;
     }
     
+    
+    public HashSet<String> readDictionary(FileResource fr){
+        HashSet<String> dictionary = new HashSet<String>();
+        for(String line : fr.lines()){
+            line = line.toLowerCase();
+            dictionary.add(line);
+        }
+        return dictionary;
+    }
+    
+    public int countWords(String message, HashSet<String> dictionary){
+        ArrayList<String> list = new ArrayList<String>();
+        int totalWord = 0;
+        for(String word : message.split("\\W")){
+            word = word.toLowerCase();
+            if(dictionary.contains(word)){
+                totalWord ++;
+            }
+        }
+        return totalWord;
+    }
+    
+    public String breakForLanguage(String encrypted, HashSet<String> dictionary, int trigger){
+        int maxLength = 100;
+        int maxWord = 0;
+        int kLength = 0;
+        char commonletter = mostCommonCharIn(dictionary);
+        
+        for(int i = 1; i < maxLength; i ++){
+            int[] key = tryKeyLength(encrypted, i, commonletter);
+            VigenereCipher vc = new VigenereCipher(key);
+            String decrypted = vc.decrypt(encrypted);
+            int wordNumber = countWords(decrypted, dictionary);
+            if(wordNumber > maxWord){
+                maxWord = wordNumber;
+                kLength = i;
+            }
+            
+            //Only for test
+            //if(i == 38){
+            //    maxWord = wordNumber;
+            //    kLength = i;
+            //    break;
+            //}
+        }
+          
+        if(trigger == 1){
+            System.out.println("The length of key is " + kLength);
+            System.out.println("There are totally " + maxWord + " words vaild");
+        }
+        //System.out.println("The length of key is " + kLength);
+        //System.out.println("There are totally " + maxWord + " words vaild");
+        
+        return breakVigenere(kLength, encrypted, commonletter);
+    }
+    
+    public char mostCommonCharIn(HashSet<String> dictionary){
+        HashMap<Character, Integer> alphabet = new HashMap<Character, Integer>();
+        for(String k : dictionary){
+            StringBuilder word = new StringBuilder(k);
+            for(int i = 0; i < word.length(); i ++){
+                char temp = word.charAt(i);
+                if(!alphabet.containsKey(temp)){
+                    alphabet.put(temp, 1);
+                }
+                else{
+                    alphabet.put(temp, alphabet.get(temp) + 1);
+                }
+            }
+        }
+        
+        int max = 0;
+        char mostCommon = 'a';
+        for(char letter : alphabet.keySet()){
+            int letterCount = alphabet.get(letter);
+            if(letterCount > max){
+                max = letterCount;
+                mostCommon = letter;
+            }
+        }   
+        return mostCommon;
+    }
+    
+    public void breakForAllLangs(String message, HashMap<String, HashSet<String>> languages){
+        HashMap<String, Integer> allLangsCount = new HashMap<String, Integer>();
+        for(String language : languages.keySet()){
+            HashSet<String> dictionary = languages.get(language);
+            String decrypted = breakForLanguage(message, dictionary, 0);
+            int wordNumber = countWords(decrypted, dictionary);
+            allLangsCount.put(language, wordNumber);
+        }
+        
+        int max = 0;
+        String finalLangs = "";
+        for(String language : allLangsCount.keySet()){
+            int wordCount = allLangsCount.get(language);
+            if(wordCount > max){
+                max = wordCount;
+                finalLangs = language;
+            }
+        }
+        
+        System.out.println("Final languages is " + finalLangs + ", with word counts as "
+         + max);
+        
+        HashSet<String> dictionary = languages.get(finalLangs);
+        String decrypted = breakForLanguage(message, dictionary, 1);
+        System.out.println(decrypted.substring(0,200));
+    }
+    
+    
+    public void main(){
+        FileResource fr = new FileResource("secretmessage3.txt");
+        String encrypted = fr.asString();
+        //HashSet<String> dictionary = readDictionary(new FileResource());
+        HashMap<String, HashSet<String>> languages = new HashMap<String, HashSet<String>>();
+        
+        DirectoryResource dr = new DirectoryResource();
+        for (File f : dr.selectedFiles()) {
+            // process each file in turn
+            FileResource frs = new FileResource(f);
+            languages.put(f.getName(), readDictionary(frs));
+        }
+        breakForAllLangs(encrypted, languages);
+    }
 }
